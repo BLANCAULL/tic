acacfdba3aeb63c54cd0dd7e6a7e8523e68c6550
Jorge Calvo <jorge@europeanvalley.es>
2018-11-29 12:42:29 +0100

    Create array.php

---
+++ php/array.php
@@ -0,0 +1,10 @@
+<?php
+
+$coches = array("Volvo", "BMW", "Toyota"); //Creo un array llamado coches con 3 valores (Volvo, BMW y Toyota)
+
+//Imprimo esos tres valores, recordando la posición que ocupa cada uno en el array 
+// para mostrar el valor del array es : $nombredelarray[posición del valor que quiero sacar]
+echo "Me gusta " . $coches[0] . ", " . $coches[1] . " and " . $coches[2] . ".";
+
+//Si quieres recorrer un array recuerda que lo puedes hacer con un bucle, usando una variable como posición de cada elemento
+?>
