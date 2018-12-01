c7cc757736da2564dc4d5ca0876797239ce50b8e
Jorge Calvo <jorge@europeanvalley.es>
2018-12-01 18:51:50 +0100

    Update array.php

--- php/array.php
+++ php/array.php
@@ -4,7 +4,19 @@ $coches = array("Volvo", "BMW", "Toyota"); //Creo un array llamado coches con 3 
 
 //Imprimo esos tres valores, recordando la posición que ocupa cada uno en el array 
 // para mostrar el valor del array es : $nombredelarray[posición del valor que quiero sacar]
+echo "<h2>Imprimir el contenido con variables</h2>";
 echo "Me gusta " . $coches[0] . ", " . $coches[1] . " and " . $coches[2] . ".";
 
+
 //Si quieres recorrer un array recuerda que lo puedes hacer con un bucle, usando una variable como posición de cada elemento
+//Recorrer el array con un bucle FOR
+echo "<h2>Imprimir el contenido con un bucle</h2>";
+
+$longitud = sizeof($coches);
+
+for ($i = 0; $i <= $longitud; $i++) {
+    echo $coches[$i];
+    echo "<br>";
+}
+
 ?>
