6a8ad407b4829876534c48d02c5de9123129db4c
Jorge Calvo <jorge@europeanvalley.es>
2018-12-16 18:32:32 +0100

    Create index.php

---
+++ php/reCAPTCHA/index.php
@@ -0,0 +1,27 @@
+<html lang="es">
+    <head>
+            <title>Acceso Clientes</title>
+            <link rel="stylesheet" type="text/css" href="./php/formulario.css">
+    </head>
+ 
+    <body>
+            <h1>Acceso Clientes</h1>
+            <h2>Practica reCAPTCHA</h2>
+            <form action="acceso1.php" method="POST">
+                    <p>
+                            <label>Nombre de usuario en EuropeanValley </label>
+                            <input type="text" name="username" /></br></br>
+                            <label>Password </label>
+                            <input type="password" name="password" />
+                    </p>
+                    <br>
+                    <br>
+                    <h3>Confirmame que no eres un robot</h3><br>
+                    
+                    <img src="http://www.drodd.com/images15/6-17.png" width="5%"><label> + </label><input type="number" name=numero1><label> = </label><input type="number" name=numero2>
+                    <br>
+                            <input type="submit" value="Acceso" />
+                    </p>
+            </form>
+    </body>
+</html>
